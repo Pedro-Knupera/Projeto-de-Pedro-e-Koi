5e4396ecfac080eaeb78ad14d321f2180d0ede46
Pedro <Pedro@sg.srv>
2021-03-01 21:48:12 -0300

    Pedro e koi

---
+++ src/components/paginas_menus/menuComponte.tsx
@@ -0,0 +1,13 @@
+import styles from '../../styles/menuContainer.module.css'
+import {Chama} from './pagina1';
+
+export function MenuFunção(){
+    
+    
+    return(
+        
+         <div className={styles.menuContainer}>
+           <a href="">{Chama}</a>
+        </div>
+    )
+}
---
+++ src/components/paginas_menus/pagina1.tsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/components/pagina1.module.css';
+
+export const Chama = <a>Pagina 1</a> ;
+
+export function pagina1(){
+
+    return(
+        <div className={styles.Pagina1Container}>
+            <h1>asdasdasda</h1>
+        </div>
+    )
+}
